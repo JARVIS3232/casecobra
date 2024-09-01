a4cf9a545d49dbcc9b08e474d2f110077e161abb
CodeSigma <pratyushshah314@gmail.com>
2024-09-01 15:43:25 +0530

    initial commit

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,38 +1,3 @@
-// import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-
-// export const GET = handleAuth();
-import { NextRequest, NextResponse } from "next/server";
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export async function GET(req: NextRequest) {
-  const authResult = await handleAuth(req as unknown as Request);
-
-  const response = NextResponse.json(authResult);
-
-  response.headers.set(
-    "Access-Control-Allow-Origin",
-    "https://casecobra-eight-delta.vercel.app"
-  );
-  response.headers.set(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  response.headers.set(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization"
-  );
-
-  return response;
-}
-export async function OPTIONS(req: NextRequest) {
-  const response = new NextResponse(null, {
-    status: 204,
-    headers: {
-      "Access-Control-Allow-Origin": "https://casecobra-eight-delta.vercel.app",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    },
-  });
-
-  return response;
-}
+export const GET = handleAuth();
